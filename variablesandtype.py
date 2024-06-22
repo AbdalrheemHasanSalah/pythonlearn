5a1833ba569d569b679045c16faa046bd5bd4f8e
56165abd123 <abdsalahhasan2003@gmail.com>
2024-06-22 12:55:42 +0300

    Add files via upload

---
+++ variablesandtype.py
@@ -0,0 +1,40 @@
+# first code in python
+# variables and output with type()
+
+print("hello python")
+
+help("keywords")
+
+print(type(1)) #int
+print(type(1.2)) #float
+print(type(-1.5)) #float
+print(type(.589)) #float 
+print(type("hello python"))#string
+print(type('a'))#string
+print(type([1,2,3,4,5]))#list
+print(type((1,2,3,4,5)))# tuple
+print(type({"one":1,"two":2,"three":3}))#dict Dictionaries
+print(type(1==1))# bool
+Name="ABD"
+name="abd"
+name="abd"
+myName="abd"
+my_name="abd"
+print(Name)
+print(name)
+print(myName)
+print(my_name)
+
+x=10
+x="hello"
+print(x)
+
+a,b,c=1,2,3
+
+print(a)
+print(b)
+print(c)
+
+
+
+
