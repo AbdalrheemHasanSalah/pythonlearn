12295c0253cada8f1a167aa609a8809e16ec072f
abdalrheem salah <abdsalahhasan2003@gmail.com>
2025-07-31 11:18:37 +0300

    Add files via upload

--- Flask/static/js/main.js
+++ Flask/static/js/main.js
@@ -1,1 +1,9 @@
-
+$(function() {
+    // Function to dynamically set the width of the skill progress bars
+        $('.skill-progress span').each(function() {
+            $(this).animate({
+                width: $(this).data('width') 
+            }, 90000);
+        });
+    }
+)
